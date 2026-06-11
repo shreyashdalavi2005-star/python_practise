3ba726d8a8b89709b69f23a834cfd3c238b71bcc
Shreyash Dalvi <shreyashdalvi2005@gmail.com>
2026-06-11 19:59:38 +0530

    a new file

---
+++ string method and indexing.py
@@ -0,0 +1,37 @@
+#name = input("Enter your name: ")
+
+#result = len(name)
+#result = name.find("a")
+#result = name.count("s")
+#result = name.rfind("s")
+#result = name.isalpha()
+#result = name.isdigit()
+#print(result)
+
+#username = input("Enter your username: ")
+
+#if len(username) <= 12 and username.isalpha() == True and username.isdigit() == False:
+#    print(f"welcome {username}")
+#else:
+#    print("please enter a valid username")
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+#Indexing =accessing the element of a sequence using [] (indexing operator)
+#          [start : end : step]
+
+#debit_number = "1234-5678-9876-0988"
+#print(debit_number[0:3])
+#print(debit_number[ :5])
+#print(debit_number[-1])
+#print(debit_number[ : :2])
+#print(debit_number[0:8:2])
+
+#--------------------------------------------------------------------------------------------------
+
+#       Reversing the number
+
+#credit_number= "1234-4567-7890-0987"
+#print(credit_number[ : :-1])
