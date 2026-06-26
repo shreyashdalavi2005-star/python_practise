a1b329e202e4e160e02a9e10f98a3b09d7c80dad
Shreyash Dalvi <shreyashdalvi2005@gmail.com>
2026-06-26 12:37:46 +0530

    changes

---
+++ LOOPS.py
@@ -0,0 +1,17 @@
+#While Loop = execute some code while some condition remain true
+
+#num = int(input("Enter a number from(1 to 10): "))
+
+#while num >10 or num <1 :
+#   print("Invalid Input")
+#    num = int(input("Enter a number from(1 to 10): "))
+#print(f"{num} is valid")
+
+#-------------------------------------------------------------------------------------------------
+
+#i = 1
+#while i <= 1000:
+#    print(i)
+#    i = i + 1
+
+#--------------------------------------------------------------------------------------------------
